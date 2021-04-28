6e7f5ebaff1f3442e4893768935bdc15ed7bee5b
Keshav Chhaparia <chhapariakeshav@gmail.com>
2021-04-28 17:56:31 +0530

    Animated BirdContainer using framer-motion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styled, { keyframes } from "styled-components";
 import React from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Container = styled.div`
 	background-image: linear-gradient(
@@ -21,83 +21,65 @@ const birdAnimation = keyframes`
     }
 `;
 
-const birdContainerAnim = keyframes`
-  0% {
-		transform: scale(0.3) translateX(-10vw);
-	}
-	
-	10% {
-		transform: translateY(2vh) translateX(10vw) scale(0.4);
-	}
-	
-	20% {
-		transform: translateY(0vh) translateX(30vw) scale(0.5);
-	}
-	
-	30% {
-		transform: translateY(4vh) translateX(50vw) scale(0.6);
-	}
-	
-	40% {
-		transform: translateY(2vh) translateX(70vw) scale(0.6);
-	}
-	
-	50% {
-		transform: translateY(0vh) translateX(90vw) scale(0.6);
-	}
-	
-	60% {
-		transform: translateY(0vh) translateX(110vw) scale(0.6);
-	}
-	
-	100% {
-		transform: translateY(0vh) translateX(110vw) scale(0.6);
-	}
-	
-}
-
-`;
-
-const Bird = styled.div`
+const Bird = styled(motion.div)`
 	background-image: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/174479/bird-cells-new.svg");
 	background-size: auto 100%;
 	width: 88px;
 	height: 125px;
 	will-change: background-position;
 	animation-name: ${birdAnimation};
-	animation-timing-function: steps(10);
 	animation-iteration-count: infinite;
+	animation-timing-function: steps(10);
 	animation-duration: 1s;
-	animation-delay: -0.5s;
 `;
 
-const BirdContainer = styled.div`
+const BirdContainer = styled(motion.div)`
 	position: absolute;
 	top: 20%;
 	left: -10%;
 	will-change: transform;
-	animation-name: ${birdContainerAnim};
-	animation-timing-function: linear;
-	animation-iteration-count: infinite;
 	transform: scale(0) translateX(-10vw);
-	animation-duration: 15s;
-	animation-delay: 0;
 `;
 
 function App() {
+	const x = ["-10vw", "10vw", "30vw", "50vw", "70vw", "90vw", "110vw", "110vw"];
+	const y = ["2vh", "2vh", "0vh", "4vh", "2vh", "0vh", "0vh", "0vh"];
+	const scale = [0.3, 0.4, 0.5, 0.6, 0.6, 0.6, 0.6, 0.6];
+
 	return (
 		<Container>
-			<BirdContainer>
+			<BirdContainer
+				animate={{
+					x: x,
+					y: y,
+					scale: scale,
+					transition: {
+						duration: 15,
+						ease: "linear",
+						repeat: Infinity,
+						repeatType: "loop",
+					},
+				}}
+			>
 				<Bird></Bird>
 			</BirdContainer>
-			<BirdContainer>
-				<Bird></Bird>
-			</BirdContainer>
-			<BirdContainer>
-				<Bird></Bird>
-			</BirdContainer>
-			<BirdContainer>
-				<Bird></Bird>
+			<BirdContainer
+				animate={{
+					x: x,
+					y: y,
+					scale: scale,
+					transition: {
+						duration: 16,
+						delay: 1,
+						ease: "linear",
+						repeat: Infinity,
+						repeatType: "loop",
+					},
+				}}
+			>
+				<Bird
+					style={{ animationDuration: "0.9s", animationDelay: "-0.75s" }}
+				></Bird>
 			</BirdContainer>
 		</Container>
 	);
